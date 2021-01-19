7db8d76ceae0d63c81116938134ae4e081dfc47d
Sulatskov Sergey <sulatskov.s@r-mades.com>
2021-01-19 18:57:12 +0300

    добавлен профиль фрагмент

--- app/src/main/java/ru/sulatskov/unsplashapp/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/ru/sulatskov/unsplashapp/ui/profile/ProfileViewModel.kt
@@ -1,13 +1,13 @@
-package ru.sulatskov.unsplashapp.ui.dashboard
+package ru.sulatskov.unsplashapp.ui.profile
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class ProfileViewModel : ViewModel() {
 
   private val _text = MutableLiveData<String>().apply {
-    value = "This is dashboard Fragment"
+    value = "This is Profile Fragment"
   }
   val text: LiveData<String> = _text
 }
